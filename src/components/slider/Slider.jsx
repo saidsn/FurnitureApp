cd0a84013ab7fc67aa8411debfdb23a37b39306a
saidsn <saidsn@code.edu.az>
2023-08-10 16:41:03 +0400

    Fixed MainHeader Component

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Slider.scss';
+
+const Slider = () => {
+    return (
+        <div className="container">
+            <div className="slider">
+                <div className="slider__content">
+                    <div className='sliedr__content--item'>your content</div>
+                    <div className='sliedr__content--item'>your content</div>
+                    <div className='sliedr__content--item'>your content</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Slider;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import MainHeader from '../../components/mainheader/MainHeader';
+import Slider from '../../components/slider/Slider';
 
 const Home = () => {
     return (
-       <MainHeader />
+        <>
+            <MainHeader />
+            <Slider />
+        </>
     )
 }
 
