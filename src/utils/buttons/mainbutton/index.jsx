d83474e4889847ea38628340d6ed8e7c7c5a1785
saidsn <saidsn@code.edu.az>
2023-08-19 18:53:26 +0400

    Fixed SlideShow Count Every Page

---
+++ src/utils/buttons/mainbutton/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MainButton.scss';
+
+
+const MainButton = () => {
+  return (
+    <div>
+        <button className="main__button">
+            Log In
+        </button>
+    </div>
+  )
+}
+
+export default MainButton;
